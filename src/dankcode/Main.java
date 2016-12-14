87b3dbe8af211aeae5483f8583e3367a87679885
Patrick <pwblais@icloud.com>
2016-12-14 08:49:02 -0500

    work was done

--- src/dankcode/Main.java
+++ src/dankcode/Main.java
@@ -12,6 +12,8 @@ import java.awt.Font;
 import javax.swing.JTextField;
 
 public class Main {
+	
+	static int topleft, topmid, topright, midleft, midmid, midright, botleft, botmid, botright = 0;
 
 	private JFrame frame;
 
@@ -66,49 +68,56 @@ public class Main {
 				 * 
 				 */
 				int currentPlayer = 1;
+				frame.setLayout(new GridLayout(3,3));
 				frame.getContentPane().remove(playerOption); //Clears previous layout
 				frame.getContentPane().remove(btn1Player); 
 				frame.getContentPane().remove(btn2Players); 
+				frame.revalidate();
+				frame.repaint();
 				
-				JButton btnNewButton = new JButton("");
-				btnNewButton.addActionListener(new ActionListener() {
+				
+				JButton topleft = new JButton("");
+				topleft.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent e) {
+						
+						
+						
 					}
 				});
-				btnNewButton.setBounds(77, 335, 140, 140);
-				frame.getContentPane().add(btnNewButton);
+				topleft.setBounds(77, 335, 140, 140);
+				frame.getContentPane().add(topleft);
 				
-				JButton button_1 = new JButton("");
-				button_1.setBounds(227, 335, 140, 140);
-				frame.getContentPane().add(button_1);
+				JButton topmid = new JButton("");
+				topmid.setBounds(227, 335, 140, 140);
+				frame.getContentPane().add(topmid);
 				
-				JButton button_2 = new JButton("");
-				button_2.setBounds(377, 335, 140, 140);
-				frame.getContentPane().add(button_2);
+				JButton topright = new JButton("");
+				topright.setBounds(377, 335, 140, 140);
+				frame.getContentPane().add(topright);
 				
-				JButton button_3 = new JButton("");
-				button_3.setBounds(377, 184, 140, 140);
-				frame.getContentPane().add(button_3);
+				JButton midleft = new JButton("");
+				midleft.setBounds(377, 184, 140, 140);
+				frame.getContentPane().add(midleft);
 				
-				JButton button_4 = new JButton("");
-				button_4.setBounds(227, 184, 140, 140);
-				frame.getContentPane().add(button_4);
+				JButton midmid = new JButton("");
+				midmid.setBounds(227, 184, 140, 140);
+				frame.getContentPane().add(midmid);
 				
-				JButton button_5 = new JButton("");
-				button_5.setBounds(77, 184, 140, 140);
-				frame.getContentPane().add(button_5);
+				JButton midright = new JButton("");
+				midright.setBounds(77, 184, 140, 140);
+				frame.getContentPane().add(midright);
 				
-				JButton button_6 = new JButton("");
-				button_6.setBounds(77, 32, 140, 140);
-				frame.getContentPane().add(button_6);
+				JButton botleft = new JButton("");
+				botleft.setBounds(77, 32, 140, 140);
+				frame.getContentPane().add(botleft);
 				
-				JButton button_7 = new JButton("");
-				button_7.setBounds(227, 32, 140, 140);
-				frame.getContentPane().add(button_7);
+				JButton botmid = new JButton("");
+				botmid.setBounds(227, 32, 140, 140);
+				frame.getContentPane().add(botmid);
 				
-				JButton button_8 = new JButton("");
-				button_8.setBounds(377, 33, 140, 140);
-				frame.getContentPane().add(button_8);
+				JButton botright = new JButton("");
+				botright.setBounds(377, 33, 140, 140);
+				frame.getContentPane().add(botright);
 				
 				
 				
@@ -117,12 +126,7 @@ public class Main {
 		btn1Player.setBounds(0, 67, 217, 147);
 		frame.getContentPane().add(btn1Player);
 		
-		JButton button = new JButton("");
-		button.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-			}
-		});
-		frame.getContentPane().add(button);
+
 		
 		
 	}
